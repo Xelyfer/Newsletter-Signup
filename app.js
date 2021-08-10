097c5a7e83252c82efa9c46071d11d05583e8f24
Xelyfer <Xelyfer@gmail.com>
2021-08-10 23:58:42 +1200

    try again

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const https = require("https");
 const request = require("request");
 
 const app = express();
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
